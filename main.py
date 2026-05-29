4b84c90c78a4cd69dbb7694bb32676e1233a8f03
Wallace <wallacevictor@etik.com>
2026-05-29 09:43:52 +0000

    Refactor main.py to improve performance
    Removed unused user agents and added resource blocking for faster loading.

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from playwright.sync_api import sync_playwright
 from seleniumbase import sb_cdp
 from scrapers.superu import SuperUScraper
-import random
 
-
-'''USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-]'''
 
 def main():
     sb = sb_cdp.Chrome()
@@ -20,22 +12,32 @@ def main():
         context = browser.contexts[0]
 
         page = context.new_page()
+
+        # Bloquer les ressources inutiles pour accélérer le chargement
+        page.route(
+            "**/*",
+            lambda route: route.abort()
+            if route.request.resource_type in [
+                "image",
+                "media",
+                "font"
+            ]
+            else route.continue_()
+        )
         
 
         # Changez variable "Fournisseur" pour tester différents scrapers
         Fournisseur = "SuperU" 
         
-        if Fournisseur == "SuperU": # OK
+        if Fournisseur == "SuperU":
             try:
                 scraper = SuperUScraper(page)
                 scraper.run()
-                page.screenshot(path="verif_superu.png", full_page=True)
                 scraper.save_to_csv("donnees_supermarches/donnees_superu.csv")
             except Exception as e:
                 print(f"❌ Erro: {e}")
                 page.screenshot(path="error_superu.png", full_page=True)
         
-        context.tracing.stop(path="trace.zip")
         browser.close()
 
 if __name__ == "__main__":
